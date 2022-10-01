fd6bb5537afb2afa720055a85e4a939bd6ee0a47
Leonardo <leonardomelegari@gmail.com>
2022-10-01 09:58:59 -0300

    feat: modal structure and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoHeader from "../../assets/logo-header.svg";
 
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoHeader} />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
